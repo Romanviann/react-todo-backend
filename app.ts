47e25345b90cc0cf4afd670f7a7ac1911867d55c
Omar Jawhar <omarjawhar.10@gmail.com>
2024-04-15 01:52:45 -0400

    re-write app.ts and added mongodb/mongoose

--- app.ts
+++ app.ts
@@ -1,42 +1,31 @@
-import express, {Application, NextFunction} from "express";
-import {errorHandler} from "./middleware/error/errorHandler";
-import createError = require('http-errors');
+import express, {Express} from 'express';
+import mongoose from "mongoose";
 require('dotenv').config();
-import path = require('path');
-import cookieParser = require('cookie-parser');
 import logger = require('morgan');
-import {IndexRouter} from "./routes";
-import {UserRouter} from "./routes/user";
 
-const app: Application = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const app: Express = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+function startDB() {
+    try {
+        mongoose.connect(`${process.env.DB_URL}`);
+    } catch (exception) {
+        console.log(exception);
+    }
+}
 
-const indexRouter = new IndexRouter().getRouter();
-const userRouter = new UserRouter().getRouter();
+startDB();
 
-app.use('/', indexRouter);
-app.use('/user', userRouter);
-
-// catch 404 and forward to error handler
-// app.use(function(req: Request, res: Response, next: NextFunction) {
-//   next(createError(404));
-// });
-
-// error handler
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get('/', function(request, response, next) {
+    response.send('React To-Do');
 });
 
-module.exports = app;
+app.use(express.json());
+app.use(logger("combined"));
+
+import userRouter from './routes/user';
+app.use('/user', userRouter);
+
+app.listen(process.env.PORT, () => {
+    console.log(`running at https://localhost:${process.env.PORT}`);
+});
